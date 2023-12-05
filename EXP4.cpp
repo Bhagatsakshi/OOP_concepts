4bf82f8197024d47b4f57b8639da646e7f7045a0
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2023-12-05 08:42:34 +0530

    Create EXP4.cpp

---
+++ EXP4.cpp
@@ -0,0 +1,128 @@
+//USING INHERITANCE
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Shape {
+public:
+  virtual double volume() const = 0; 
+};
+class Cube:public Shape 
+{
+public:
+  Cube(double side):side_(side) {}
+  double volume() const override {
+      return pow(side_, 3); 
+  }
+private:
+  double side_;
+};
+class Cuboid:public Shape {
+public:
+  Cuboid(double length,double width,double height) : length_(length),width_(width),height_(height)
+  {}
+  double volume() const override { return length_*width_*height_; }
+private:
+  double length_;
+  double width_;
+  double height_;
+};
+class Cylinder:public Shape {
+public:
+  Cylinder(double radius, double height):radius_(radius), height_(height) {}
+  double volume() const override { return 3.14 * pow(radius_, 2) * height_; }
+private:
+  double radius_;
+  double height_;
+};
+int main() {
+  Cube cube(3);
+  Cuboid cuboid(4,5,6);
+  Cylinder cylinder(2,7);
+  cout<<"Volume of cube: "<<cube.volume()<<endl;
+  cout<<"Volume of cuboid: "<<cuboid.volume()<<endl;
+  cout<<"Volume of cylinder: "<<cylinder.volume()<<endl;
+  return 0;
+}
+
+//USING EXCEPTION HANDLING
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+double volume(double side) {
+  if(side<=0) {
+    throw "Side must be positive";
+  }
+  return pow(side,3);
+}
+double volume(double len,double wid,double h) {
+  if(len<=0||wid<=0||h<=0) {
+    throw "Dimensions must be positive";
+  }
+  return len*wid*h;
+}
+double volume(double r,double h) {
+  if(r<= 0||h<=0) {
+    throw "Radius and height must be positive";
+  }
+  return 3.14*pow(r,2)*h;
+}
+
+int main() {
+  try {
+    cout<<"Volume of cube: "<<volume(3)<<endl;
+    cout<<"Volume of cuboid: "<<volume(4, 5, 6)<<endl;
+    cout<<"Volume of cylinder: "<<volume(2, 7)<<endl;
+  } 
+  catch(const exception& e) 
+  {
+    cerr<<"Error: "<<e.what()<<endl;
+  }
+
+  return 0;
+}
+
+//USING VIRTUAL FUNCTION
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Shape 
+{
+public:
+  virtual double volume() const = 0; 
+};
+class Cube:public Shape {
+public:
+  Cube(double side):side_(side) {}
+  double volume() const override { return side_*side_*side_; }
+private:
+  double side_;
+};
+class Cuboid:public Shape {
+public:
+  Cuboid(double length,double width,double height):length_(length), width_(width), height_(height) {}
+  double volume() const override { return length_ * width_ * height_; }
+private:
+  double length_;
+  double width_;
+  double height_;
+};
+class Cylinder : public Shape {
+public:
+  Cylinder(double radius, double height):radius_(radius), height_(height) {}
+  double volume() const override { 
+      return 3.14* pow(radius_, 2) * height_; }
+private:
+  double radius_;
+  double height_;
+};
+int main() {
+  Cube cube(3);
+  Cuboid cuboid(4,5,6);
+  Cylinder cylinder(2,7);
+  cout<<"Volume of cube: "<<cube.volume()<<endl;
+  cout<<"Volume of cuboid: "<<cuboid.volume()<<endl;
+  cout<<"Volume of cylinder: "<<cylinder.volume()<<endl;
+  return 0;
+}
