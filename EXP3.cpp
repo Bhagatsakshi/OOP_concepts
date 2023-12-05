286d0b41a410b4c460ecf5edbe6ad1a04df7a385
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2023-12-05 12:12:47 +0530

    Update EXP3.cpp

--- EXP3.cpp
+++ EXP3.cpp
@@ -105,3 +105,106 @@ int main() {
   }
   return 0;
 }
+
+
+//USING VIRTUAL FUNCTION 
+
+#include<iostream>
+using namespace std;
+class ExamResultAnalyzer
+{
+public:
+    virtual void enterTestResult(int result)=0;
+    virtual void displaySummary()=0;
+};
+
+class InteractiveExamResultAnalyzer:public ExamResultAnalyzer 
+{
+private:
+    int passedCount=0;
+    int failedCount=0;
+
+public:
+    void enterTestResult(int result) override 
+   {
+        if (result==1) 
+        {
+            passedCount++;
+        } else if (result==2) 
+        {
+            failedCount++;
+        } else {
+            cout<<"Invalid input: Result must be 1 (passed) or 2 (failed)." <<endl;
+        }
+    }
+    void displaySummary() override {
+        cout<<"Summary of Exam Results:"<<endl;
+        cout<<"Number of students who passed: "<<passedCount<<endl;
+        cout<<"Number of students who failed: "<<failedCount<<endl;
+        if(passedCount>8)
+        {
+            cout<<"Raise tuition."<<endl;
+        }
+    }
+};
+int main() {
+    InteractiveExamResultAnalyzer analyzer;
+    for (int i=0;i<10;i++) {
+        int result;
+        cout<<"Enter result (1 for passed, 2 for failed): ";
+        cin>>result;
+        analyzer.enterTestResult(result);
+    }
+    analyzer.displaySummary();
+    return 0;
+}
+
+//USING FRIEND FUNCTION 
+
+#include <iostream>
+using namespace std;
+class ExamResultAnalyzer 
+{
+private:
+    int passCount=0;
+    int failCount=0;
+
+public:
+    void enterTestResult(int result) 
+{
+        if (result==1) {
+            passCount++;
+        } else if (result==2) {
+            failCount++;
+        } else {
+            cout<<"Invalid input: Result must be 1 (passed) or 2 (failed)."<<endl;
+        }
+    }
+
+    friend void displaySummary(const ExamResultAnalyzer& analyzer);
+};
+
+void displaySummary(const ExamResultAnalyzer& analyzer)
+{
+    cout<<"Summary of Exam Results:" <<endl;
+    cout<<"Number of students who passed: "<<analyzer.passCount<<endl;
+    cout<<"Number of students who failed: "<<analyzer.failCount<<endl;
+
+    if (analyzer.passCount>8)
+    {
+        cout<<"Raise tuition."<<endl;
+    }
+}
+
+int main() {
+    ExamResultAnalyzer analyzer;
+    for (int i=0;i<10;i++) 
+    {
+        int result;
+        cout<<"Enter result (1 for passed, 2 for failed): ";
+        cin>>result;
+        analyzer.enterTestResult(result);
+    }
+    displaySummary(analyzer);
+    return 0;
+}
